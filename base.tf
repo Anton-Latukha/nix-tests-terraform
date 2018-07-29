ac1bc316a546deb59e751fbf12cc3492cae46a63
Anton-Latukha <anton.latukha@gmail.com>
2018-07-30 00:14:45 +0300

    base.tf: add docker provider, tested init

--- base.tf
+++ base.tf
@@ -1,16 +1,64 @@
-# An AMI
-variable "ami" {
-  description = "the AMI to use"
+# # An AMI
+# variable "ami" {
+#   description = "the AMI to use"
+# }
+
+# /* A multi
+#    line comment. */
+# resource "aws_instance" "web" {
+#   ami               = "${var.ami}"
+#   count             = 2
+#   source_dest_check = false
+
+#   connection {
+#     user = "root"
+#   }
+# }
+
+# Configure the Docker provider
+provider "docker" {
+  host = "tcp://127.0.0.1:2376/"
 }
 
-/* A multi
-   line comment. */
-resource "aws_instance" "web" {
-  ami               = "${var.ami}"
-  count             = 2
-  source_dest_check = false
+#### Funcking Images
 
-  connection {
-    user = "root"
-  }
-}
+
+# resource "docker_image" "debian" {
+#   name = "ubuntu:latest"
+# }
+
+
+# resource "docker_image" "ubuntu" {
+#   name = "ubuntu:latest"
+# }
+
+
+# resource "docker_image" "ubuntu" {
+#   name = "ubuntu:latest"
+# }
+
+
+# resource "docker_image" "ubuntu" {
+#   name = "ubuntu:latest"
+# }
+
+
+# resource "docker_image" "ubuntu" {
+#   name = "ubuntu:latest"
+# }
+
+
+# resource "docker_image" "ubuntu" {
+#   name = "ubuntu:latest"
+# }
+
+
+# resource "docker_image" "ubuntu" {
+#   name = "ubuntu:latest"
+# }
+
+
+# resource "docker_image" "ubuntu" {
+#   name = "ubuntu:latest"
+# }
+
