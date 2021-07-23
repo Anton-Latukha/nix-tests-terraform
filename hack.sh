f88e87e36c4c7600806ad5b131f609748ca6cb49
Tom Bereknyei <tomberek@gmail.com>
2021-07-23 13:31:28 -0400

    alpine requires different user setup

--- hack.sh
+++ hack.sh
@@ -3,3 +3,11 @@
 
 groupadd -r nixbld
     for n in $(seq 1 10); do useradd -c "Nix build user $n" -d /var/empty -g nixbld -G nixbld -M -N -r -s "$(command -v nologin)" "nixbld$n"; done
+
+if ! getent group nixbld ; then
+    addgroup -g 30000 -S nixbld
+
+    for i in $(seq -w 1 30); do
+        adduser -S -D -h /var/empty -g "Nix build user $i" -u 300"$i" -G nixbld "nixbld$i"
+    done
+fi
